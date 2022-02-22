0c89a1eb7e15b3a46de955018766ed4f38eec95f
Voldemort175 <reubenjohns17@gmail.com>
2022-02-22 23:38:15 +0900

    Settings updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4ag-==t(5u9&k(931du5hjh0!hki7nac1$5e!m&d+@=u1k)d=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['voldblogapp.herokuapp.com']
 
 
 # Application definition
